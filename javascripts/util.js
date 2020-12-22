7a5436ba445520e6d9d285066dce5773e5fee93b
Buck4437 <Buck4437@users.noreply.github.com>
2020-12-23 02:14:22 +0800

    Bug fix :(

---
+++ javascripts/util.js
@@ -0,0 +1,32 @@
+function copyText(text){
+    //source: https://www.30secondsofcode.org/blog/s/copy-text-to-clipboard-with-javascript
+    const el = document.createElement('textarea');
+    el.value = text;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    try{
+        document.execCommand('copy');
+        console.log("Auto-copy successful");
+    } catch(e){
+        console.log("Auto-copy unsuccessful");
+        prompt("Failed to Auto-copy. Please copy manually:", text);
+    }
+    document.body.removeChild(el);
+}
+
+function download(filename, text) {
+    //source: https://www.bitdegree.org/learn/javascript-download
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
