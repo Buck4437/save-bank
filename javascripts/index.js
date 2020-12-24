63bc0bbca7fb3e80d56ed6a6063b0a142fa071e4
Buck4437 <Buck4437@users.noreply.github.com>
2020-12-25 02:33:35 +0800

    Added colours and themes

--- javascripts/index.js
+++ javascripts/index.js
@@ -9,33 +9,51 @@ var app = new Vue({
         saves,
         C,
         currentCategoryIndex: 0,
-        currentTab: C.FILES_TAB
+        currentTab: C.FILES_TAB,
+        themes: ["Light", "Dark"],
+        currentTheme: 1
     },
-    computed:{
-        currentCategory(){
+    computed: {
+        currentCategory() {
             return this.saves[this.currentCategoryIndex]
         }
     },
-    methods:{
-        openTab(tab){
+    methods: {
+        switchTheme(index) {
+            if (index !== undefined) {
+                this.currentTheme = index;
+            } else {
+                this.currentTheme++;
+                if (this.currentTheme >= this.themes.length) {
+                    this.currentTheme = 0;
+                }
+            }
+            for (let theme of this.themes) {
+                document.body.classList.remove(theme.toLowerCase() + "-theme");
+            }
+            document.body.classList.add(this.themes[this.currentTheme].toLowerCase() + "-theme");
+            localStorage.setItem("saveBankTheme", this.currentTheme)
+        },
+        openTab(tab) {
             this.currentTab = tab;
         },
-        switchCategory(index){
+        switchCategory(index) {
             this.openTab(this.C.FILES_TAB);
             this.currentCategoryIndex = index;
-            // if (this.currentCategory.saves.length != 0){
-            //     this.displayedText = this.currentCategory.saves[0].data
-            // } else {
-            //     this.displayedText = "No save file available"
-            // }
         },
-        copyByPath(saveFile){
+        copyByPath(saveFile) {
             copyText(saveFile.data);
         },
-        downloadFile(saveFile){
+        downloadFile(saveFile) {
             let filename = saveFile.name + ".txt";
             let text = saveFile.data;
             download(filename, text);
+        }
+    },
+    mounted() {
+        let theme = localStorage.getItem("saveBankTheme")
+        if (theme !== undefined) {
+            this.switchTheme(Number(theme));
         }
     }
 })
