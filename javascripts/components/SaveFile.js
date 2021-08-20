5510b735dc540c891f3e8f36f6ee6275736d4e41
Buck4437 <Buck4437@users.noreply.github.com>
2021-08-20 16:52:54 +0800

    refactor

--- javascripts/components/SaveFile.js
+++ javascripts/components/SaveFile.js
@@ -1,28 +1,18 @@
 Vue.component("save-file", {
     props: {
-        saveFile: Object
-    },
-    methods: {
-        copyText() {
-            File.copyText(this.saveFile.data);
-        },
-        exportFile() {
-            const filename = `${this.saveFile.name}.txt`;
-            const text = this.saveFile.data;
-            File.download(filename, text);
-        }
+        saveFile: Save
     },
     template: `
     <div class="file-con">
         <div class="file-text-con">
             <div class="file-name">{{saveFile.name}}</div>
-            <div class="pre-formatted file-desc">{{saveFile.desc || "No description provided."}}</div>
+            <div class="pre-formatted file-desc">{{saveFile.desc}}</div>
         </div>
         <div class="file-btn-con">
-            <tooltip-button class="file-btn" tooltip="Copied!" @click="copyText">
+            <tooltip-button class="file-btn" tooltip="Copied!" @click="saveFile.copy()">
                 Copy to Clipboard
             </tooltip-button>
-            <button class="file-btn" @click="exportFile">
+            <button class="file-btn" @click="saveFile.export()">
                 Export as .txt
             </button>
         </div>
