8bf17c63c355231b81e7552e5d6dded15bcc51a6
Buck4437 <Buck4437@users.noreply.github.com>
2021-09-25 18:08:26 +0800

    patch 2

--- javascripts/update.js
+++ javascripts/update.js
@@ -1,17 +1,13 @@
 if (!localStorage.getItem("saveBankData")) {
     const newData = {
-        customSaves: [],
         settings: {
-            theme: 0
+            theme: Theme.defaultTheme
         },
         saveVersion: 1
     };
 
     if (localStorage.getItem("saveBankTheme")) {
         newData.settings.theme = JSON.parse(localStorage.getItem("saveBankTheme"));
-    }
-    if (localStorage.getItem("saveBankCustomSaves")) {
-        newData.customSaves = JSON.parse(localStorage.getItem("saveBankCustomSaves")).saves;
     }
 
     localStorage.setItem("saveBankData", JSON.stringify(newData));
