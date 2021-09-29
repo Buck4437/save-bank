6444887724b6c2d251644c6a49ceff943b43f90a
Buck4437 <Buck4437@users.noreply.github.com>
2021-09-29 17:38:34 +0800

    Changelog

--- javascripts/index.js
+++ javascripts/index.js
@@ -9,7 +9,7 @@ const app = new Vue({
             saveVersion: 1
         },
         currentTab: "",
-        version: "Beta 7"
+        version: "Beta 7A"
     },
     computed: {
         tabs() {
