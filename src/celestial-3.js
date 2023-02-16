44902345f6c00db0d71da79c30d3ee53efeffc6e
Buck4437 <51261544+Buck4437@users.noreply.github.com>
2023-02-16 19:04:08 +0800

    Added new category + renaming

--- src/celestial-3.js
+++ src/celestial-3.js
@@ -3,9 +3,8 @@ import Category from "./classes/Category";
 import Save from "./classes/Save";
 
 export const celestial1 = new Category({
-    name: "Celestial 1",
+    name: "Celestial 3",
     color: "celestial",
-    desc: "Celestial 1 - Celestial 3",
     placeholder: "Coming soon!",
     saves: [
     ]
