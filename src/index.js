996900e5dad8fa74a9052ebfc2f3d0a80c16371b
Buck4437 <51261544+Buck4437@users.noreply.github.com>
2023-03-10 22:24:15 +0800

    Reorganized dilation saves

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { eternity } from "./eternity";
 import { postMilestone } from "./post-milestone";
 import { eternityChallenge } from "./eternity-challenge";
 import { timeDilation } from "./time-dilation";
-import { postE4k } from "./post-e4k-ep";
 import { reality } from "./reality";
 import { celestial1 } from "./celestial-1";
 import { celestial3 } from "./celestial-3";
@@ -25,7 +24,6 @@ const saves = [
     postMilestone,
     eternityChallenge,
     timeDilation,
-    postE4k,
     reality,
     celestial1,
     celestial3,
